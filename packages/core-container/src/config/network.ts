8986f2bfc657280ce14ff1a2c89d3f281ee6fe84
TGCChain <51577372+TGCChain@users.noreply.github.com>
2019-07-06 12:50:30 +0800

    Add files via upload

---
+++ packages/core-container/src/config/network.ts
@@ -0,0 +1,29 @@
+import { NetworkManager } from "@arkecosystem/crypto";
+import expandHomeDir from "expand-home-dir";
+import Joi from "joi";
+import { resolve } from "path";
+import { schemaNetwork } from "./schema";
+
+export class Network {
+    /**
+     * Expose information about the for the operating network to the environment.
+     * @return {void}
+     */
+    public static setUp(opts: any) {
+        const config = NetworkManager.findByName(opts.network);
+
+        const { error } = Joi.validate(config, schemaNetwork);
+
+        if (error) {
+            throw new Error(
+                `An invalid network configuration was provided or is inaccessible due to it's security settings. ${
+                    error.message
+                }.`,
+            );
+        }
+
+        process.env.CORE_NETWORK_NAME = config.network.name;
+
+        return config;
+    }
+}
